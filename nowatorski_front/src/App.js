869e77040b6f7055e0373d44a8f00a16f0379df9
milosz.kucharski <miloszkucharski@gmail.com>
2024-11-22 23:31:49 +0100

    front

---
+++ nowatorski_front/src/App.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+
+function App() {
+  const [stats, setStats] = useState({});
+
+  useEffect(() => {
+    const fetchStats = async () => {
+      const response = await fetch('http://localhost:8000/api/stats');
+      const data = await response.json();
+      setStats(data);
+    };
+
+    fetchStats();
+    const interval = setInterval(fetchStats, 5000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div>
+      <h1>Statystyki odwiedzin</h1>
+      <ul>
+        {Object.entries(stats).map(([url, count]) => (
+          <li key={url}>
+            {url}: {count} odwiedzin
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
